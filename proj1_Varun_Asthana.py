c6824eb0f61e48f7beadcf6fbb0e917f2e2c463f
Varun <varunasthana92@gmail.com>
2020-02-21 19:38:36 -0500

    code block added to iterate new sates untill goal is reached

--- proj1_Varun_Asthana.py
+++ proj1_Varun_Asthana.py
@@ -87,6 +87,68 @@ def main():
         print('Input state is same as goal state')
         return
 
+    # writing initial data of state, ownID and parentID to the respective txt files
+    nodes= open("Nodes.txt", 'w')
+    NodesInfo= open("NodesInfo.txt", 'w')
+    data= cp.deepcopy((parentState).reshape(3,3))
+    data= data.transpose()
+    data= data.reshape(-1)
+    content= str(data[0]) + " " + str(data[1]) + " " + str(data[2]) + " "
+    content+= str(data[3]) + " " + str(data[4]) + " " + str(data[5]) + " "
+    content+= str(data[6]) + " " + str(data[7]) + " " + str(data[8]) + "\n"
+    nodes.write(content)
+    content2= str(parentNode.ownID) + " " + str(parentNode.pID) + "\n"
+    NodesInfo.write(content2)
+
+    print('Processing...')
+    idx= 0
+
+    # iterations to reach to the final state
+    while(found != True):
+        # current node is termed as parent node
+        parentNode= graph.getNode(idx)
+
+        # finding the position of blank tile (represented by 0)
+        zeroPos= np.where(parentNode.state == 0)
+
+        # to reduce computations of going back to same node from where the current node
+        # is obtained, and compare if it was unique or not (which in all case will not be unique)
+        # checking position of zero of parent of current node
+        pastZero= np.where((graph.getNode(parentNode.pID)).state==0)
+
+        # checking all possible set of actions
+        all_actions= action(zeroPos[0][0])
+        for step in all_actions:
+            # ignoring the state which will lead to parent state of current node
+            if(step != pastZero):
+                tempNode= cp.deepcopy(parentNode)
+                tempNode.state[zeroPos[0][0]]= tempNode.state[step]
+                tempNode.state[step]= 0
+                tempNode.pID= parentNode.ownID
+                uniqueID= graph.unique(tempNode.state)
+                if(uniqueID != -1):
+                    tempNode.ownID= uniqueID
+                    graph.push(tempNode)
+                    data= cp.deepcopy((tempNode.state).reshape(3,3))
+                    data= data.transpose()
+                    data= data.reshape(-1)
+                    content= str(data[0]) + " " + str(data[1]) + " " + str(data[2]) + " "
+                    content+= str(data[3]) + " " + str(data[4]) + " " + str(data[5]) + " "
+                    content+= str(data[6]) + " " + str(data[7]) + " " + str(data[8]) + "\n"
+                    nodes.write(content)
+
+                    content2= str(tempNode.ownID) + " " + str(tempNode.pID) + "\n"
+                    NodesInfo.write(content2)
+
+                    # check if the final state is achieved
+                    compare= tempNode.state == finalState
+                    if(compare.all()):
+                        found =True
+                        print('Solved')
+                        break
+        idx+=1
+    nodes.close()
+
 if __name__ == '__main__':
     # calling main function
     main()
